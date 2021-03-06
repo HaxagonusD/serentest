0c6b9b1ebe480f9f6a3d5c8b3d8290f718c77ea6
HaxagonusD <jquezad1@binghamton.edu>
2021-03-06 01:36:20 -0500

    Second stage works

--- app.js
+++ app.js
@@ -86,7 +86,149 @@ app.post("/hello", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-app.post("/slack/interactivity", (req, res) => {});
+app.post("/slack/interactive", (req, res) => {
+  let { payload } = req.body;
+  payload = JSON.parse(payload);
+
+  console.log("Parsed Payload ", payload);
+  res.status(200).end();
+  const whichResponse = {
+    feeling: () => {
+      web.chat
+        .postMessage({
+          channel: payload.container.channel_id,
+          attachments: [],
+          text: "When are you free for a walk?",
+          user: payload.user.id,
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "plain_text",
+                text: "What time are you free for a walk?\n",
+              },
+              accessory: {
+                action_id: "time",
+                type: "multi_static_select",
+                max_selected_items: 2,
+                placeholder: {
+                  type: "plain_text",
+                  text: "Time",
+                },
+                options: [
+                  {
+                    text: { type: "plain_text", text: "12:00" },
+                    value: "12:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "12:30" },
+                    value: "12:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "13:00" },
+                    value: "13:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "13:30" },
+                    value: "13:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "14:00" },
+                    value: "14:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "14:30" },
+                    value: "14:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "15:00" },
+                    value: "15:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "15:30" },
+                    value: "15:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "16:00" },
+                    value: "16:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "16:30" },
+                    value: "16:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "17:00" },
+                    value: "17:00",
+                  },
+                  {
+                    text: { type: "plain_text", text: "17:30" },
+                    value: "17:30",
+                  },
+                  {
+                    text: { type: "plain_text", text: "18:00" },
+                    value: "18:00",
+                  },
+                ],
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "plain_text",
+                text: " What day are you free for a walk?\n",
+              },
+              accessory: {
+                type: "multi_static_select",
+                placeholder: { type: "plain_text", text: "Select a Day" },
+                action_id: "day",
+                options: [
+                  {
+                    text: { type: "plain_text", text: "Monday" },
+                    value: "Monday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Tuesday" },
+                    value: "Tuesday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Wednesday" },
+                    value: "Wednesday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Thursday" },
+                    value: "Thursday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Friday" },
+                    value: "Friday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Saturday" },
+                    value: "Saturday",
+                  },
+                  {
+                    text: { type: "plain_text", text: "Sunday" },
+                    value: "Sunday",
+                  },
+                ],
+              },
+            },
+          ],
+        })
+        .catch((err) => console.log(err));
+    },
+    time: () => {
+      /*save response to the database*/
+    },
+    day: () => {},
+    hobbies: () => {},
+    numbers: () => {},
+  };
+
+  whichResponse[payload.actions[0].action_id]
+    ? whichResponse[payload.actions[0].action_id]()
+    : res.status(500);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
